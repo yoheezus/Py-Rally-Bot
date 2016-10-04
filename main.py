8fdf523a7c641754d603a9b54f699f7d6770a6c9
yoheezus <yoheezus@users.noreply.github.com>
2016-10-04 20:53:09 +0200

    Update and rename main.py to non-asyncio/main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from reconstruct import Rally
-
-ssid = 'r2q3h7pgg925f68745f3957349dh2newo3wwosrubqt8aqix'
-rally = Rally('208.85.93.114', ssid, '36931745', 'Yoheezus',
-              'ava/a1/88/32c19cd223388a1_M_6.00_11_1465590975', '45002')
-                                                 #Change to your avatar using 102 or 107^            #roomid ^
-
---
+++ non-asyncio/main.py
@@ -0,0 +1,7 @@
+from reconstruct import Rally
+
+ssid = 'ENTER SSID'
+rally = Rally('IP_ADDR', ssid, 'GAIA_ID', 'USERNAME',
+              'AVATAR_URL', 'ROOM_ID')
+#Change to your avatar using 102 or 107^            #roomid ^
+
