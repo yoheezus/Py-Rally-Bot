9ad432f9b35743cd20c4f09035968bd65e02b1de
yoheezus <yoheezus@users.noreply.github.com>
2016-10-05 12:05:42 +0200

    Delete main.py

--- non-asyncio/main.py
+++
@@ -1,7 +0,0 @@
-from reconstruct import Rally
-
-ssid = 'ENTER SSID'
-rally = Rally('IP_ADDR', ssid, 'GAIA_ID', 'USERNAME',
-              'AVATAR_URL', 'ROOM_ID')
-#Change to your avatar using 102 or 107^            #roomid ^
-
