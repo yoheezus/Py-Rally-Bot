50d4f7bd260da4c5f53bf2ebc191e74a012067fa
Ryan <Butts@users.noreply.github.com>
2016-10-08 20:29:22 +0100

    Update login.py

--- asyncio/login.py
+++ asyncio/login.py
@@ -10,13 +10,14 @@ class GaiaAuth():
         finputs = re.findall('<input([^>]+)>', msource) #regex for getting the inputs
         
         for i in range(3, len(finputs)): #loop from 3 to the length of our inputs from the regex, we don't need the first 3
-            print(finputs[i])
+           # print(finputs[i])
             ftempname = re.search('name="([a-z0-9]{3,30})"', finputs[i]).group(1) #Input name
             ftempvalue = re.search('value="([\.\a-z0-9]{25,32})"', finputs[i]).group(1) #Input value
             fpostdata += ",'" + ftempname + "':'" + ftempvalue + "'" #Finally compose it all
-        #rheaders = requests.post('http://www.gaiaonline.com/auth/login/', data = {fpostdata}).headers
-            
-        print(fpostdata) #Our necessary post data.
+        rheaders = requests.post('http://www.gaiaonline.com/auth/login/', data = {fpostdata}).headers
+        print(rheaders)    
+        #print(fpostdata) #Our necessary post data.
 
 
-GaiaAuth('','') #Do nothing with this yet
+GaiaAuth('username','password') #Do nothing with this yet
+
