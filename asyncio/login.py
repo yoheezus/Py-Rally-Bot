50f7d64adefacf81098cbc96b6c5b9f9b8dae901
yoheezus <yoheezus@users.noreply.github.com>
2016-10-08 23:52:21 +0200

    changes to params

--- asyncio/login.py
+++ asyncio/login.py
@@ -7,13 +7,13 @@ class GaiaAuth():
         ftempname = ''
         ftempvalue = ''
         finputs = re.findall('<input([^>]+)>', msource) #regex for getting the inputs
+        params = {'username' : username, 'password' : password}
         
         for i in range(3, len(finputs)): #loop from 3 to the length of our inputs from the regex, we don't need the first 3
             #print(finputs[i])
             ftempname = re.search('name="([a-z0-9]{3,30})"', finputs[i]).group(1) #Input name
             ftempvalue = re.search('value="([\.\a-z0-9]{25,32})"', finputs[i]).group(1) #Input value
-            fpostdata += ftempname + ftempvalue #Finally compose it all
-        fpostdata = {'username' : username, 'password' : password, }
+            params[ftempname] = ftempvalue # adds form name and corresponding value to params dict
     '''   
     rheaders = requests.post('http://www.gaiaonline.com/auth/login/', data = fpostdata).headers
         sid = re.search('gaia55_sid=([a-z0-9]{48})', rheaders['Set-Cookie'])
