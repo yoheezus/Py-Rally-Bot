0a2541094599798f82aaec6044ad9d1bd98ca0c3
Ryan <Butts@users.noreply.github.com>
2016-10-08 19:06:40 +0100

    Update login.py
    updates to getting the input names and values

--- asyncio/login.py
+++ asyncio/login.py
@@ -3,12 +3,16 @@ import re
 
 class GaiaAuth():
     def __init__(self, username, password):
-        fpostdata = 'username=' + username + '&password=' + password + '&'
-        msource = requests.get('http://gaiaonline.com/').text
-        print(msource)
-        finputs = re.findall('<input([^>]+)>', msource)
-        for i in range(3, len(finputs)):
-            print(finputs)
+        fpostdata = 'username=' + username + '&password=' + password #Start of out post data
+        msource = requests.get('http://gaiaonline.com/').text #Send HTTP GET request to http://gaiaonline.com/
+        ftempname = ''
+        ftempvalue = ''
+        finputs = re.findall('<input([^>]+)>', msource) #regex for getting the inputs
+        for i in range(3, len(finputs)): #loop from 3 to the length of our inputs from the regex, we don't need the first 3
+            ftempname = re.search('name="(.*?)"', finputs[i]).group(1) #Input name
+            ftempvalue = re.search('value="(.*?)"', finputs[i]).group(1) #Input value
+            fpostdata += '&' + ftempname + '=' + ftempvalue #Finally compose it all
+        print(fpostdata) #Our necessary post data.
 
 
-GaiaAuth('','')
+GaiaAuth('','') #Do nothing with this yet
