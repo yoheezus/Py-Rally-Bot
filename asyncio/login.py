3811a37d514cc5ca0214a96dcbf6dcef2ead7363
Ryan <Butts@users.noreply.github.com>
2016-10-08 20:08:52 +0100

    Update login.py
    Optimized regex

--- asyncio/login.py
+++ asyncio/login.py
@@ -4,14 +4,15 @@ import re
 class GaiaAuth():
     def __init__(self, username, password):
         fpostdata = "'username':'" + username + "','password':'" + password + "'" #Start of out post data
-        msource = requests.get('http://gaiaonline.com/').text #Send HTTP GET request to http://gaiaonline.com/
+        msource = requests.get('http://gaiaonline.com/').text.replace('data-value', '') #Send HTTP GET request to http://gaiaonline.com/
         ftempname = ''
         ftempvalue = ''
         finputs = re.findall('<input([^>]+)>', msource) #regex for getting the inputs
         
         for i in range(3, len(finputs)): #loop from 3 to the length of our inputs from the regex, we don't need the first 3
-            ftempname = re.search('name="(.*?)"', finputs[i]).group(1) #Input name
-            ftempvalue = re.search('value="(.*?)"', finputs[i]).group(1) #Input value
+            print(finputs[i])
+            ftempname = re.search('name="([a-z0-9]{3,30})"', finputs[i]).group(1) #Input name
+            ftempvalue = re.search('value=\"([\.\a-z0-9]{25,32})\"', finputs[i]).group(1) #Input value
             fpostdata += ",'" + ftempname + "':'" + ftempvalue + "'" #Finally compose it all
         #rheaders = requests.post('http://www.gaiaonline.com/auth/login/', data = {fpostdata}).headers
             
